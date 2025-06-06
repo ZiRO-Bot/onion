ee07db0b6125621d41f669e3dc32f69c56ffaed3
Ahmad Ansori Palembani <palembani@gmail.com>
2025-06-06 18:46:32 +0700

    feat: ScheduleView

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import contextlib
+import logging
+from typing import override
 import asqlite
 import asyncio
 import bot
@@ -7,6 +10,7 @@ import sys
 import re
 from bot import bot_owner_id, client, tree, database_required
 from src import publisher
+from logging.handlers import RotatingFileHandler
 
 
 token = os.environ.get("DISCORD_TOKEN")
@@ -32,10 +36,50 @@ async def on_message(message: discord.Message) -> None:
 
 @client.event
 async def on_ready() -> None:
-    print(f"Logged in as {client.user}")
+    log = logging.getLogger()
+    log.info(f"Logged in as {client.user}")
+
+class RemoveNoise(logging.Filter):
+    def __init__(self):
+        super().__init__(name='discord.state')
+
+    @override
+    def filter(self, record: logging.LogRecord) -> bool:
+        if record.levelname == 'WARNING' and 'referencing an unknown' in record.msg:
+            return False
+        return True
+
+@contextlib.contextmanager
+def setup_logging():
+    log = logging.getLogger()
+
+    try:
+        discord.utils.setup_logging()
+        # __enter__
+        max_bytes = 32 * 1024 * 1024  # 32 MiB
+        logging.getLogger('discord').setLevel(logging.INFO)
+        logging.getLogger('discord.http').setLevel(logging.WARNING)
+        logging.getLogger('discord.state').addFilter(RemoveNoise())
+
+        log.setLevel(logging.INFO)
+        handler = RotatingFileHandler(filename='data/onion.log', encoding='utf-8', mode='w', maxBytes=max_bytes, backupCount=5)
+        dt_fmt = '%Y-%m-%d %H:%M:%S'
+        fmt = logging.Formatter('[{asctime}] [{levelname:<7}] {name}: {message}', dt_fmt, style='{')
+        handler.setFormatter(fmt)
+        log.addHandler(handler)
+
+        yield
+    finally:
+        # __exit__
+        handlers = log.handlers[:]
+        for hdlr in handlers:
+            hdlr.close()
+            log.removeHandler(hdlr)
 
 @database_required
 async def migrate_db_if_needed() -> None:
+    log = logging.getLogger()
+
     current_version = (await bot.database.fetchone("SELECT user_version FROM pragma_user_version"))["user_version"]
     migration_version = 0
     to_execute: list[str] = []
@@ -46,7 +90,7 @@ async def migrate_db_if_needed() -> None:
         try:
             migration_version = int(f.rstrip(".sql"))
         except:
-            print(f"Migration failed, unable to get version from {f}")
+            log.exception(f"Migration failed, unable to get version from {f}")
             return
 
         if migration_version <= current_version:
@@ -55,15 +99,16 @@ async def migrate_db_if_needed() -> None:
         to_execute.append(f)
 
     if migration_version > current_version:
-        print(f"Migrating database from {current_version} to {migration_version}")
+        log.info(f"Migrating database from {current_version} to {migration_version}")
         for f in to_execute:
             await bot.database.executescript(open(f"migrations/{f}").read())
 
     await bot.database.execute(f"PRAGMA user_version = {migration_version}")
 
 async def start_bot() -> None:
+    log = logging.getLogger()
     if not token:
-        print("DISCORD_TOKEN is not set!")
+        log.exception("DISCORD_TOKEN is not set!")
         sys.exit(1)
 
     await publisher.register_commands()
@@ -76,7 +121,8 @@ async def start_bot() -> None:
     await client.connect()
 
 def main() -> None:
-    asyncio.run(start_bot())
+    with setup_logging():
+        asyncio.run(start_bot())
 
 
 if __name__ == "__main__":
--- src/publisher.py
+++ src/publisher.py
@@ -1,3 +1,4 @@
+import logging
 import aiohttp
 import asyncio
 import bot
@@ -25,19 +26,26 @@ current_release: ReleaseData | None = None
 task: asyncio.Task[None] | None = None
 have_data = asyncio.Event()
 
-async def restart():
+async def restart() -> None:
     global task
+    log = logging.getLogger()
+
     if task:
         _ = task.cancel()
+    log.info(f"{'Restart' if task else 'Start'}ing release publisher...")
     task = client.loop.create_task(dispatch())
+
+def create_embed(data: ReleaseData) -> discord.Embed:
+    embed = discord.Embed()
+    embed.title = data.title
+    if data.thumbnail_url:
+        embed.set_thumbnail(url=data.thumbnail_url)
+    return embed
 
 async def publish_release(data: ReleaseData) -> None:
     channel = client.get_partial_messageable(publish_channel_id)
-    embed = discord.Embed()
-    embed.title = data.title
+    embed = create_embed(data)
     embed.description = f"Episode {data.episode} just released!"
-    if data.thumbnail_url:
-        embed.set_thumbnail(url=data.thumbnail_url)
     await channel.send(embeds=[embed])
     await bot.database.execute("UPDATE OR IGNORE releases SET published = 1 WHERE id = ?", (data.id,))
 
@@ -75,12 +83,15 @@ async def get_release(days: int) -> ReleaseData | None:
 
 async def wait(days: int) -> ReleaseData | None:
     global current_release
+    log = logging.getLogger()
 
+    log.info("Getting earliest release")
     release: ReleaseData | None = await get_release(days)
     if release is not None:
         have_data.set()
         return release
 
+    log.info("Waiting for new release...")
     have_data.clear()
     current_release = None
     _ = await have_data.wait()
@@ -94,6 +105,8 @@ async def get_latest_schedule():
     """
     Get schedule data from AniChart
     """
+    log = logging.getLogger()
+
     query = """
     query($weekStart: Int, $weekEnd: Int) {
       Page {
@@ -141,9 +154,7 @@ async def get_latest_schedule():
                             current_airing_at,
                         ),
                     )
-                    if not earliest_airing_at:
-                        earliest_airing_at = current_airing_at
-                    elif current_airing_at < earliest_airing_at:
+                    if not earliest_airing_at or current_airing_at < earliest_airing_at:
                         earliest_airing_at = current_airing_at
     if earliest_airing_at is None:
         return
@@ -151,13 +162,78 @@ async def get_latest_schedule():
     have_data.set()
     if current_release and earliest_airing_at < int(current_release.publish_at.timestamp()):
         # This is unlikely to happened, but just in case...
-        print("Found earlier release, restarting...")
+        log.info("Found earlier release, restarting...")
         await restart()
 
+#region Views
+class ScheduleView(discord.ui.View):
+    def __init__(self, releases: list[ReleaseData], timeout=900):
+        super().__init__(timeout=timeout)
+        self.releases = releases
+        self.current_index = 0
+
+        # Disable buttons if only one image
+        if len(releases) <= 1:
+            self.previous_button.disabled = True
+            self.next_button.disabled = True
+    
+    def render(self):
+        data = self.releases[self.current_index]
+
+        embed = create_embed(data)
+        embed.description = f"Episode {data.episode} release at <t:{int(data.publish_at.timestamp())}> (<t:{int(data.publish_at.timestamp())}:R>)"
+        embed.set_footer(text=f"Page {self.current_index + 1}/{len(self.releases)}")
+
+        return embed
+    
+    @discord.ui.button(label='◀', style=discord.ButtonStyle.primary)
+    async def previous_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if self.current_index > 0:
+            self.current_index -= 1
+        else:
+            self.current_index = len(self.releases) - 1  # Loop to last imag
+
+        embed = self.render()
+        return await interaction.response.edit_message(embed=embed, view=self)
+    
+    @discord.ui.button(label='▶', style=discord.ButtonStyle.primary)
+    async def next_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if self.current_index < len(self.releases) - 1:
+            self.current_index += 1
+        else:
+            self.current_index = 0  # Loop to first image
+
+        embed = self.render()
+        return await interaction.response.edit_message(embed=embed, view=self)
+    
+    @discord.ui.button(label='❌', style=discord.ButtonStyle.danger)
+    async def close_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        # Disable all buttons and edit message
+        for item in self.children:
+            item.disabled = True
+
+        embed = self.render()
+        await interaction.response.edit_message(embed=embed, view=self)
+        self.stop()
+#endregion
+
+#region Commands
 async def schedules(interaction: discord.Interaction):
     rows = await bot.database.fetchall(f"SELECT * FROM releases WHERE published == 0 ORDER BY publish_at")
-    data = [row["title"] for row in rows]
-    await interaction.response.send_message(data, ephemeral=True)
+    data = [
+        ReleaseData(
+            row["id"],
+            row["title"],
+            row["episode"],
+            row["thumbnail_url"],
+            datetime.datetime.fromtimestamp(row["publish_at"], tz=datetime.timezone.utc),
+            row["published"] == 1,
+        ) for row in rows
+    ]
+
+    view = ScheduleView(data)
+    embed = view.render()
+    await interaction.response.send_message(embeds=[embed], view=view)
 
 async def fetch_schedules(interaction: discord.Interaction):
     await get_latest_schedule()
@@ -171,5 +247,6 @@ async def register_commands():
             callback = callback,
         )
 
-    tree.add_command(command("schedules", callback=schedules))
-    tree.add_command(command("fetch-schedules", callback=fetch_schedules))
+    tree.add_command(command("schedules", description="Get current anime release schedule", callback=schedules))
+    tree.add_command(command("fetch-schedules", description="Fetch anime release schedule from AniList", callback=fetch_schedules))
+#endregion
